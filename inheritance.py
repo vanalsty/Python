e07dae7446dbf8a541a79715a52769e5b93ed92b
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-06-11 13:16:33 -0700

    Python Step 174

---
+++ inheritance.py
@@ -0,0 +1,15 @@
+class Family:
+    name = ' '
+    age = ' '
+    height = ' '
+    hairColor = ' '    
+
+class Children(Family):
+    schoolYear = ' '
+    clothesSize = ' '
+
+class Parents(Family):
+    employment = ' '
+    email = ' '
+
+
