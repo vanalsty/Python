557f1ebb340baf0d4a3f7f065da9f33ddae36d99
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-06-15 16:10:47 -0700

    Python Step 181
    take 2

--- pythonStep181.py
+++ pythonStep181.py
@@ -10,9 +10,9 @@ class Llama(Animal):
     def talk(self):
         print(self.name, "hums!")
     def looks(self):
-        print(self.name, "has soft, fluffy fiber!")
-    def action(self):
-        print(self.name, "spits when mad.")
+        furColor = "brown"
+        height = "6 feet"
+        print(self.name, "has soft,", furColor, ",fluffy fiber!")
 
 class Pig(Animal):
     def __init__(self, name=""):
@@ -20,9 +20,11 @@ class Pig(Animal):
     def talk(self):
         print(self.name, "says Oink!")
     def play(self):
-        print(self.name, "rolls in mud!")
-    def eats(self):
-        print(self.name, "eats slop.")
+        skinColor = "pink"
+        weight = "500 lbs"
+        print(self.name, "is", skinColor, "weighs", weight, "and rolls in mud!")
+
+
 
 a = Animal()
 a.talk()
@@ -30,12 +32,12 @@ a.talk()
 l = Llama("Rojo")
 l.talk()
 l.looks()
-l.action()
+
 
 p = Pig("Babe")
 p.talk()
 p.play()
-p.eats()
+
 
 input()
     
