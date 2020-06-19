457a1eb7a0d63535c391b6018614fdfabedd74e6
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-06-19 10:16:06 -0700

    Python Step 200

--- PythonStep200.py
+++ PythonStep200.py
@@ -1,23 +1,67 @@
+from abc import ABC, abstractmethod
 
-from abc import ABC,abstractmethod
-class Animal(ABC):
-    @abstractmethod
-    def eat(self):
-        pass
+class Basic_info(ABC):
+    def __init__(self):
+        self.name=""
+        self.age =0
+        self.u_id=0
+    def accept(self):
+        self.name = input("Enter Name:  ")
+        self.age = int(input("Enter Age:  "))
+        self.user_id = input("Enter UserID:  ")
+    def display(self):
+        print(self.name)
+        print(self.age)
+        print(self.user_id)
         
-class Llama(Animal):
-    def eat(self):
-        print("Llama eats hay.")
+    @abstractmethod
+    def Accept_Basic_info(self):
+        pass
 
-class Dog(Animal):
-    def eat(self):
-        print("Dog eats kibble.")
+    @abstractmethod
+    def Display_basic_info(self):
+        pass
+
+class student(Basic_info):
+    def __init__(self):
+        self.roll_no = 0
+        self.Class = 0
+        self.section = 0
 
 
+    def Accept_Basic_info(self):
+        self.accept()
+        self.roll_no = input("Enter roll No:  ")
+        self.Class = input("Enter class:  ")
+        self.section = input("Enter section:  ")
 
-l=Llama()
-l.eat()
+    def Display_basic_info(self):
+        self.display()
+        print(self.roll_no)
+        print(self.Class)
+        print(self.section)
 
-d=Dog()
-d.eat()
+class teacher(Basic_info):
+    def __init__(self):
+        self.subject = 0
+        self.shift = 0
 
+    def Accept_Basic_info(self):
+        self.accept()
+        self.subject = input("Enter Subject: ")
+        self.shift = input("Enter Shift: ")
+
+    def Display_basic_info(self):
+        self.display()
+        print(self.subject)
+        print(self.shift)
+
+a=student()
+print("Student")
+a.Accept_Basic_info()
+a.Display_basic_info()
+
+b=teacher()
+print("Teacher")
+b.Accept_Basic_info()
+b.Display_basic_info()
